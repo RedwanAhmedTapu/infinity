4eeaafa8126a314667f5fb3bfe18d9e5097d7133
RedwanAhmedTapuu <redwan25880@gmail.com>
2023-05-07 02:13:07 +0600

    cart changed

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -26,25 +26,32 @@ const Cart = () => {
   };
   return (
     <>
-      <div className="w-screen flex flex-col justify-center items-center self-center  gap-y-4  bg-slate-500 p-8 ">
-        {cart.map((curElem, index) => {
-          return <CartItem key={index} {...curElem} />;
-        })}
-        <div className=" totalcart flex justify-center items-center space-x-2">
-          <div
-            className="  sm:text-4xl text-zinc-400 w-34 sm:w-64 h-16 bg-pink-900   rounded shadow-xl  uppercase p-2 flex justify-center items-center"
-            style={{ fontSize: "1.5rem" }}
-          >
-            total item : {total_item}$
-          </div>
-          <div
-            className="  sm:text-4xl text-zinc-400 w-34 sm:w-64 h-16 bg-pink-900 rounded shadow-xl uppercase p-2 flex justify-center items-center"
-            style={{ fontSize: "1.5rem" }}
-          >
-            total price : {total_price}$
+      {cart ? (
+        <div className="w-screen flex flex-col justify-center items-center self-center  gap-y-4  bg-slate-500 p-8 ">
+          {cart &&
+            cart.map((curElem, index) => {
+              return <CartItem key={index} {...curElem} />;
+            })}
+          <div className=" totalcart flex justify-center items-center space-x-2">
+            <div
+              className="  sm:text-4xl text-zinc-400 w-34 sm:w-64 h-16 bg-pink-900   rounded shadow-xl  uppercase p-2 flex justify-center items-center"
+              style={{ fontSize: "1.5rem" }}
+            >
+              total item : {total_item}$
+            </div>
+            <div
+              className="  sm:text-4xl text-zinc-400 w-34 sm:w-64 h-16 bg-pink-900 rounded shadow-xl uppercase p-2 flex justify-center items-center"
+              style={{ fontSize: "1.5rem" }}
+            >
+              total price : {total_price}$
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="flex w-96 h-48 bg-slate-700 text-3xl text-white flex justify-center items-center ">
+          Your Cart is empty
+        </div>
+      )}
     </>
   );
 };
