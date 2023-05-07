d06177af032e00938b2b1fae10eb1b891e16ab35
RedwanAhmedTapuu <redwan25880@gmail.com>
2023-05-07 20:23:20 +0600

    cartContext cart changed

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -13,8 +13,8 @@ const getLocalCartData = () => {
 };
 
 const initialState = {
-  // cart: [],
-  cart: getLocalCartData(),
+  cart: [],
+  // cart: getLocalCartData(),
   total_item: "",
   total_price: "",
 };
